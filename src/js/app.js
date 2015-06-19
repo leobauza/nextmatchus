05d8ec1c361ce43c1a1f8cc8cc0ea5648aa582ad
Leo Bauza <leob@njimedia.com>
2015-06-19 11:30:45 -0400

    task(responsive) Add height responsive ref

--- src/js/app.js
+++ src/js/app.js
@@ -11,3 +11,43 @@ var overlay = new Overlay();
 var social = new Social(document, {
   clock: clock
 });
+
+
+var winHeight = $(window).height(),
+    bottomVal = parseInt($('.ref-wrap').css('bottom')),
+    diff = 0;
+
+if (winHeight < 1000) {
+  diff = 1000 - winHeight;
+  $('.ref-wrap').css({
+    'bottom': (bottomVal - diff) + "px"
+  });
+}
+
+if (winHeight > 1000) {
+  diff = winHeight - 1000;
+  $('.ref-wrap').css({
+    'bottom': (bottomVal + diff) + "px"
+  });
+}
+
+
+$(window).resize(function () {
+
+  var newHeight = $(window).height();
+
+  if (newHeight < 1000) {
+    diff = 1000 - newHeight;
+    $('.ref-wrap').css({
+      'bottom': (bottomVal - diff) + "px"
+    });
+  }
+
+  if (newHeight > 1000) {
+    diff = newHeight - 1000;
+    $('.ref-wrap').css({
+      'bottom': (bottomVal + diff) + "px"
+    });
+  }
+
+});
--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -12,6 +12,46 @@ var overlay = new Overlay();
 var social = new Social(document, {
   clock: clock
 });
+
+
+var winHeight = $(window).height(),
+    bottomVal = parseInt($('.ref-wrap').css('bottom')),
+    diff = 0;
+
+if (winHeight < 1000) {
+  diff = 1000 - winHeight;
+  $('.ref-wrap').css({
+    'bottom': (bottomVal - diff) + "px"
+  });
+}
+
+if (winHeight > 1000) {
+  diff = winHeight - 1000;
+  $('.ref-wrap').css({
+    'bottom': (bottomVal + diff) + "px"
+  });
+}
+
+
+$(window).resize(function () {
+
+  var newHeight = $(window).height();
+
+  if (newHeight < 1000) {
+    diff = 1000 - newHeight;
+    $('.ref-wrap').css({
+      'bottom': (bottomVal - diff) + "px"
+    });
+  }
+
+  if (newHeight > 1000) {
+    diff = newHeight - 1000;
+    $('.ref-wrap').css({
+      'bottom': (bottomVal + diff) + "px"
+    });
+  }
+
+});
 },{"./libs/flyweight":2,"./modules/clock":3,"./modules/overlay":4,"./modules/social":5}],2:[function(require,module,exports){
 /**
  * The Flyweight Class
