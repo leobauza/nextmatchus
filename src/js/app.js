53e10d9abbd0aa4b0a028ec7b4d16891faf0e905
Leo Bauza <leob@njimedia.com>
2015-06-30 15:22:26 -0400

    task(tweaks) Ref pops up from the bottom

--- src/js/app.js
+++ src/js/app.js
@@ -19,58 +19,95 @@ var social = new Social(document, {
 
 function heightAdjustments() {
 
-  var winHeight = $(window).height(),
-      winWidth = $(window).width(),
-      bottomVal = parseInt($('.ref-wrap').css('bottom')),
-      diff = 0;
+  var headerHeight = $('.main__header').outerHeight(),
+      winWidth = $(window).width();
 
-  if (winHeight < 1000) {
-    diff = 1000 - winHeight;
+
+  if (winWidth > 850) {
     $('.ref-wrap').css({
-      'bottom': (bottomVal - diff) + "px"
+      'top': headerHeight + 50 + 'px'
+    });
+  } else {
+    $('.ref-wrap').css({
+      'top': headerHeight + 10 + 'px'
     });
   }
 
-  if (winHeight > 1000) {
-    diff = winHeight - 1000;
-    $('.ref-wrap').css({
-      'bottom': (bottomVal + diff) + "px"
-    });
-  }
 
   $(window).resize(function () {
 
-    var newHeight = $(window).height(),
-        newWidth = $(window).width(),
-        throttle = Math.abs(newHeight - winHeight) > 20;
+    var newHeaderHeight = $('.main__header').outerHeight(),
+        newWinWidth = $(window).width();
 
-    if (winWidth > 850 && newWidth <= 850) {
-      throttle = true;
-      winWidth = newWidth;
-      bottomVal = -250;
-    } else if (winWidth <= 850 && newWidth > 850) {
-      throttle = true;
-      winWidth = newWidth;
-      bottomVal = -350;
-    }
 
-    if (newHeight < 1000 && throttle) {
-      diff = 1000 - newHeight;
+    if (winWidth > 850 && newWinWidth < 850) {
       $('.ref-wrap').css({
-        'bottom': (bottomVal - diff) + "px"
+        'top': newHeaderHeight + 10 + 'px'
       });
-      winHeight = newHeight;
     }
 
-    if (newHeight > 1000 && throttle) {
-      diff = newHeight - 1000;
+    if (winWidth <= 850 && newWinWidth > 850) {
       $('.ref-wrap').css({
-        'bottom': (bottomVal + diff) + "px"
+        'top': newHeaderHeight + 50 + 'px'
       });
-      winHeight = newHeight;
     }
+
+    winWidth = newWinWidth;
 
   });
+
+  // var winHeight = $(window).height(),
+  //     winWidth = $(window).width(),
+  //     bottomVal = parseInt($('.ref-wrap').css('bottom')),
+  //     diff = 0;
+  //
+  // if (winHeight < 1000) {
+  //   diff = 1000 - winHeight;
+  //   $('.ref-wrap').css({
+  //     'bottom': (bottomVal - diff) + "px"
+  //   });
+  // }
+  //
+  // if (winHeight > 1000) {
+  //   diff = winHeight - 1000;
+  //   $('.ref-wrap').css({
+  //     'bottom': (bottomVal + diff) + "px"
+  //   });
+  // }
+  //
+  // $(window).resize(function () {
+  //
+  //   var newHeight = $(window).height(),
+  //       newWidth = $(window).width(),
+  //       throttle = Math.abs(newHeight - winHeight) > 20;
+  //
+  //   if (winWidth > 850 && newWidth <= 850) {
+  //     throttle = true;
+  //     winWidth = newWidth;
+  //     bottomVal = -250;
+  //   } else if (winWidth <= 850 && newWidth > 850) {
+  //     throttle = true;
+  //     winWidth = newWidth;
+  //     bottomVal = -350;
+  //   }
+  //
+  //   if (newHeight < 1000 && throttle) {
+  //     diff = 1000 - newHeight;
+  //     $('.ref-wrap').css({
+  //       'bottom': (bottomVal - diff) + "px"
+  //     });
+  //     winHeight = newHeight;
+  //   }
+  //
+  //   if (newHeight > 1000 && throttle) {
+  //     diff = newHeight - 1000;
+  //     $('.ref-wrap').css({
+  //       'bottom': (bottomVal + diff) + "px"
+  //     });
+  //     winHeight = newHeight;
+  //   }
+  //
+  // });
 
 }
 
