2d5c965dc2c697240b112c1ace2ea92f963d4654
Leo Bauza <leob@njimedia.com>
2015-07-05 15:53:09 -0400

    task(craft) Buildout homepage tpl

--- craft/plugins/myplugin/twigextensions/MyPluginTwigExtension.php
+++ craft/plugins/myplugin/twigextensions/MyPluginTwigExtension.php
@@ -24,6 +24,7 @@ class MyPluginTwigExtension extends \Twig_Extension
       'myplugin'  => new \Twig_Function_Method($this, 'myplugin', array('is_safe' => array('html'))),
       'makeNumberScreen'  => new \Twig_Function_Method($this, 'makeNumberScreen', array('is_safe' => array('html'))),
       'makeSecondsScreen'  => new \Twig_Function_Method($this, 'makeSecondsScreen', array('is_safe' => array('html'))),
+      'makeDateObject'  => new \Twig_Function_Method($this, 'makeDateObject', array('is_safe' => array('html'))),
     );
   }
 
@@ -191,6 +192,89 @@ class MyPluginTwigExtension extends \Twig_Extension
 
     return $output;
 
+  }
+
+  public function makeDateObject ($raw_date)
+  {
+
+    date_default_timezone_set ('America/New_York');
+    $raw_time = strtotime($raw_date);
+
+    /**
+     * Get the moment the client loads the site
+     * Get the moment of the next match
+     * Make both into DateTime objects
+     * Compare the two
+     */
+    $today = date('Y-m-d H:i:s');
+    $date = date('Y-m-d H:i:s e', $raw_time);
+    $date2 = new DateTime($today);
+    $date1 = new DateTime($date);
+    $interval = $date1->diff($date2, false);
+
+    /**
+     * Compare the dates to make sure it is in the future
+     * @todo
+     *   - Actually compare stuff
+     *   - This should actually be in a loop with a bunch of dates
+     *     to compare before making the DateTime objects? Compare
+     *     unix timestamps probably
+     */
+    $countdown_status = FALSE; // the countdown is OFF
+    $time_diff = array();
+    if ($date1 > $date2) {
+
+      // print "difference " . $interval->d . " days " . $interval->h . " hours " . $interval->i . " minutes " . $interval->s . " seconds<br>";
+      $countdown_status = TRUE; // the countdown is ON
+
+      /**
+       * Create arrays for hours and minutes
+       * The counter requires these numbers separately for efficiency.
+       * If the number is under ten then the first number is 0
+       */
+      if ($interval->h < 10) {
+        $hoursTil =  array(0, $interval->h);
+      } else {
+        $hoursTil =  str_split($interval->h);
+      }
+      if ($interval->i < 10) {
+        $minsTil =  array(0, $interval->i);
+      } else {
+        $minsTil =  str_split($interval->i);
+      }
+
+      $time_diff = array(
+        "days" => $interval->d,
+        "hours" => $hoursTil,
+        "mins" => $minsTil,
+        "seconds" => ($interval->s === 0)? 60 : $interval->s,
+      );
+
+
+    } else {
+
+      $countdown_status = FALSE; // the countdown is OFF
+      $time_diff = array(
+        "days" => 0,
+        "hours" => array(0, 0),
+        "mins" => array(0, 0),
+        "seconds" => 0, // only way to be 0 is this way meaning game is ON
+      );
+
+    }
+
+
+    $time_data = array(
+      'info' => array(
+        'raw' => $raw_date,
+        'est' => date('g:i A', $raw_time),
+        'date' => date('d M Y', $raw_time),
+      ),
+      'status' => $countdown_status, // FALSE if OFF, TRUE if ON
+      'diff' => $time_diff,
+    );
+
+    return $time_data;
   }
 
 }
