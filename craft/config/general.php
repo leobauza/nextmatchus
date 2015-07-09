840d2185e8fd3c44fb4f4a2c8868fe328582ecfa
Leo Bauza <leob@njimedia.com>
2015-07-09 10:24:40 -0400

    task(staging) Set up staging env variables

--- craft/config/general.php
+++ craft/config/general.php
@@ -4,7 +4,8 @@
  * General Configuration
  *
  * All of your system's general configuration settings go in here.
- * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
+ * You can see a list of the default settings in
+ * craft/app/etc/config/defaults/general.php
  */
 
 return array(
@@ -15,6 +16,22 @@ return array(
 
   'nextmatch.loc' => array(
     'devMode' => true,
+    'siteUrl' => 'http://nextmatch.loc/',
+    'environmentVariables' => array(
+      'basePath' => '/var/www/vhosts/nextmatch.us/www/base/web/',
+      'baseUrl'  => 'http://nextmatch.loc/',
+    )
+  ),
+
+  'nextmatch.leobauza.com' => array(
+    'siteUrl' => 'http://nextmatch.leobauza.com/',
+    'environmentVariables' => array(
+      'basePath' => '/home/leobauza/subdomain/nextmatch/web/',
+      'baseUrl'  => 'http://nextmatch.leobauza.com/',
+    )
+  ),
+
+  'nextmatch.us' => array(
     'siteUrl' => 'http://nextmatch.loc/',
     'environmentVariables' => array(
       'basePath' => '/var/www/vhosts/nextmatch.us/www/base/web/',
